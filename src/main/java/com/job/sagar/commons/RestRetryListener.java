f8fcebe98c135188bd1e8792139bfe3c272cf0be
Mht <mhtarora17@gmail.com>
2024-05-29 19:58:03 +0530

    Retry Listener

---
+++ src/main/java/com/job/sagar/commons/RestRetryListener.java
@@ -0,0 +1,26 @@
+package com.job.sagar.commons;
+
+
+import com.commons.utility.CommonsUtility;
+import org.apache.Logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.listener.RetryListenerSupport;
+
+public class RestRetryListener extends RetryListenerSupport {
+    
+    private static final Logger log = LogManager.getLogger(RestRetryListener.class);
+
+    public RestRetryListener() {
+    }
+
+
+    public <T, E extends Throwable> void onError (RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+        try {
+            log.error("Retryable method threw {}th exception {}", context.getRetryCount(), throwable.toString());
+        } catch (Exception var5) {
+            log.error( "Exception occurred onError in retry {}", CommonsUtility.exceptionFormatter (var5));
+        }
+    }
+}
