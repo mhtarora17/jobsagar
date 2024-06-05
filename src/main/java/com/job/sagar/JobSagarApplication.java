ab17549b1ce174fac344bf5d5803e587861894c5
Mht <mhtarora17@gmail.com>
2024-06-05 16:12:59 +0530

    configs

--- src/main/java/com/job/sagar/JobSagarApplication.java
+++ src/main/java/com/job/sagar/JobSagarApplication.java
@@ -1,8 +1,14 @@
 package com.job.sagar;
 
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The type Application.
@@ -10,6 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Mohit Arora
  */
 @SpringBootApplication
+@ServletComponentScan
+@EnableScheduling
+@ComponentScan(basePackages = {"com.job.sagar"})
+@EnableAutoConfiguration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT15M")
+@EnableAsync
 public class JobSagarApplication {
 
   /**
---
+++ src/main/java/com/job/sagar/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.job.sagar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authz) -> authz.requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
